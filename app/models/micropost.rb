02173a5a25a6224f000e9f0923deb5a3d5573215
Jeremy M. Damons <jermdamons@gmail.com>
2015-03-13 12:48:22 +0200

    Finish demo app

---
+++ app/models/micropost.rb
@@ -0,0 +1,8 @@
+class Micropost < ActiveRecord::Base
+      #attr accessible :content, :user_id
+	  #attr_accessible :content, :user_id
+	  
+	  belongs_to :user
+	  
+      validates :content, :length => { :maximum => 140 }
+end
